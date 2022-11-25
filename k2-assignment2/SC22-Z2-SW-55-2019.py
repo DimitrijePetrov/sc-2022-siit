fb530364f2a8b9b8d4b6d4ff871547757d36adbe
Dimitrije Petrov <dimpet96@gmail.com>
2022-11-25 16:06:27 +0100

    almost finished

--- k2-assignment2/SC22-Z2-SW-55-2019.py
+++ k2-assignment2/SC22-Z2-SW-55-2019.py
@@ -1,9 +1,13 @@
+import sys
+import csv
+
 import numpy as np
 import cv2 # OpenCV
 import matplotlib
 import matplotlib.pyplot as plt
 import collections
 
+import pandas
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
@@ -25,7 +29,7 @@ def image_bin(image_gs):
     height, width = image_gs.shape[0:2]
     image_binary = np.ndarray((height, width), dtype=np.uint8)
     # ret, image_bin = cv2.threshold(image_gs, 127, 255, cv2.THRESH_BINARY)
-    image_bin = cv2.adaptiveThreshold(image_gs, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 185, 5)
+    image_bin = cv2.adaptiveThreshold(image_gs, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 187, 3)
     return image_bin
 
 
@@ -89,19 +93,20 @@ def winner(output):
     return max(enumerate(output), key=lambda x: x[1])[0]
 
 
-def remove_letter_holes(regions_array, img_bin):
-    no_holes_array = []
-    for i in range(0, len(regions_array)):
+def fix_letters(regions_array, img_bin):
+    no_holes_array = [regions_array[0]]
+
+    for i in range(1, len(regions_array)):
         x, y, w, h = regions_array[i][1]
         x_, y_, w_, h_ = regions_array[i - 1][1]
 
-        if not (i > 1 and x_ < x and (x + w) < (x_ + w_) and y_ < y and (y + h) < (y_ + h_)):
-            no_holes_array.append(regions_array[i])
-
         if y_ > y + h and x > x_ and (x + w) < (x_ + w_):
             no_holes_array.pop(-1)
-            i_region = img_bin[y-20:y_ + h_ + 1, x_:x_ + w_ + 1]
-            regions_array.append([resize_region(i_region), (x_, y, w_, y_ + h_ + (y_ - y))])
+            i_region = img_bin[y:y_ + h_ + 1, x_:x_ + w_ + 1]
+            no_holes_array.append([resize_region(i_region), (x_, y, w_, y_ + h_ - y)])
+
+        elif not (x_ <= x and (x + w) <= (x_ + w_) and y_ <= y and (y + h) <= (y_ + h_)):
+            no_holes_array.append(regions_array[i])
 
     return no_holes_array
 
@@ -113,18 +118,27 @@ def draw_regions(image_orig, regions_array):
     return image_orig
 
 
+def display_result(outputs, alphabet):
+    result = ""
+    for output in outputs:
+        # result.append(alphabet[winner(output)])
+        result += alphabet[winner(output)]
+    return result
+
+
 def select_roi_with_distances(image_orig, image_bin):
     contours, hierarchy = cv2.findContours(image_bin.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     regions_array = []
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
         area = cv2.contourArea(contour)
-        if (1558 > area > 247 and h > 30 and w > 15) or (15 > h > 9 and 15 > w > 9):
+        if (1600 > area > 200 and h > 30 and w > 15) or (15 > h > 9 and 15 > w > 9):
+            # print(area, ":", w, ",", h)
             region = image_bin[y:y + h + 1, x:x + w + 1]
             regions_array.append([resize_region(region), (x, y, w, h)])
 
     regions_array = sorted(regions_array, key=lambda x: x[1][0])
-    regions_array = remove_letter_holes(regions_array, image_bin)
+    regions_array = fix_letters(regions_array, image_bin)
     image_orig = draw_regions(image_orig, regions_array)
 
     sorted_regions = [region[0] for region in regions_array]
@@ -154,60 +168,84 @@ def display_result_with_spaces(outputs, alphabet, k_means):
     return result
 
 
-names = ['a1', 'e1', 'e2', 'e3', 'e4', 'l1', 'm1', 's1', 'u1', 'v1', 'w1', 'w2']
+folder_path = sys.argv[1]
+df = pandas.read_csv(folder_path + '/res.txt', sep=", ", engine="python")
+file_names = df['file'].to_list()
+correct_solutions = df['text'].to_list()
 
-# for name in names:
-#     print("\n" + name)
-#     image_color = load_image('data2/' + name + '.jpg')
-#     img = image_bin(image_gray(image_color))
-#     selected_regions, letters, region_distances = select_roi_with_distances(image_color.copy(), img)
-#     print("Broj prepoznatih regiona: ", len(letters))
-#     display_image(selected_regions)
+alphabet = [
+    'A', 'r', 'b', 'e', 'i', 't', 's', 'p', 'l', 'a', 'z', 'E', 'P', 'f', 'o', 'g', 'ss', 'L', 'n', 'w', 'M', 'k', 'S',
+    'U', 'm', 'c', 'h', 'V', 'd', 'u', 'W',
+]
+all_letters = []
 
-image_color = load_image('data2/s1.jpg')
+image_color = load_image(folder_path + '/a1.jpg')
 img = image_bin(image_gray(image_color))
-selected_regions, letters, region_distances = select_roi_with_distances(image_color.copy(), img)
-print("Broj prepoznatih regiona: ", len(letters))
-display_image(selected_regions)
-for letter in letters:
-    display_image(letter)
+_, letters, _ = select_roi_with_distances(image_color.copy(), img)
+letters.pop(10)
+all_letters.extend(letters)
 
-# image_color = load_image('data2/e2.jpg')
-# img = image_bin(image_gray(image_color))
-# selected_regions, letters, region_distances = select_roi_with_distances(image_color.copy(), img)
-# print("Broj prepoznatih regiona: ", len(letters))
-# display_image(selected_regions)
-#
-# image_color = load_image('data2/e3.jpg')
-# img = image_bin(image_gray(image_color))
-# selected_regions, letters, region_distances = select_roi_with_distances(image_color.copy(), img)
-# print("Broj prepoznatih regiona: ", len(letters))
-# display_image(selected_regions)
-#
-# image_color = load_image('data2/e4.jpg')
-# img = image_bin(image_gray(image_color))
-# selected_regions, letters, region_distances = select_roi_with_distances(image_color.copy(), img)
-# print("Broj prepoznatih regiona: ", len(letters))
-# display_image(selected_regions)
+image_color = load_image(folder_path + '/e1.jpg')
+img = image_bin(image_gray(image_color))
+_, letters, _ = select_roi_with_distances(image_color.copy(), img)
+all_letters.extend([letters[0], letters[6]])
 
-# alphabet = ['A', 'r', 'b', 'e', 'i', 't', 's', 'p', 'l', 'a', 't', 'z']
-# inputs = prepare_for_ann(letters)
-# outputs = convert_output(alphabet)
-# ann = create_ann(output_size=len(letters))
-# ann = train_ann(ann, inputs, outputs, epochs=2000)
-#
-# image_color = load_image('data2/a1.jpg')
-# img = image_bin(image_gray(image_color))
-# selected_regions, letters, distances = select_roi_with_distances(image_color.copy(), img)
-# print("Broj prepoznatih regiona: ", len(letters))
-# display_image(selected_regions)
-#
-# # neophodno je da u K-Means algoritam bude prosledjena matrica u kojoj vrste odredjuju elemente
-# distances = np.array(distances).reshape(len(distances), 1)
-#
-# k_means = KMeans(n_clusters=2)
-# k_means.fit(distances)
-#
-# inputs = prepare_for_ann(letters)
-# results = ann.predict(np.array(inputs, np.float32))
-# print(display_result_with_spaces(results, alphabet, k_means))
+image_color = load_image(folder_path + '/e2.jpg')
+img = image_bin(image_gray(image_color))
+_, letters, _ = select_roi_with_distances(image_color.copy(), img)
+all_letters.extend([letters[2], letters[3], letters[5], letters[11]])
+
+image_color = load_image(folder_path + '/l1.jpg')
+img = image_bin(image_gray(image_color))
+_, letters, _ = select_roi_with_distances(image_color.copy(), img)
+all_letters.extend([letters[0], letters[4], letters[6]])
+
+image_color = load_image(folder_path + '/m1.jpg')
+img = image_bin(image_gray(image_color))
+_, letters, _ = select_roi_with_distances(image_color.copy(), img)
+all_letters.extend([letters[0], letters[3]])
+
+image_color = load_image(folder_path + '/s1.jpg')
+img = image_bin(image_gray(image_color))
+_, letters, _ = select_roi_with_distances(image_color.copy(), img)
+all_letters.extend([letters[0]])
+
+image_color = load_image(folder_path + '/u1.jpg')
+img = image_bin(image_gray(image_color))
+_, letters, _ = select_roi_with_distances(image_color.copy(), img)
+all_letters.extend([letters[0], letters[1], letters[3], letters[4]])
+
+image_color = load_image(folder_path + '/v1.jpg')
+img = image_bin(image_gray(image_color))
+_, letters, _ = select_roi_with_distances(image_color.copy(), img)
+all_letters.extend([letters[0], letters[6], letters[7]])
+
+image_color = load_image(folder_path + '/w1.jpg')
+img = image_bin(image_gray(image_color))
+_, letters, _ = select_roi_with_distances(image_color.copy(), img)
+all_letters.extend([letters[0]])
+
+inputs = prepare_for_ann(all_letters)
+outputs = convert_output(alphabet)
+ann = create_ann(output_size=len(all_letters))
+ann = train_ann(ann, inputs, outputs, epochs=2000)
+
+# train_file_names = ['a1.jpg', 'e1.jpg', 'e2.jpg', 'e3.jpg', 'e4.jpg', 'l1.jpg', 'm1.jpg', 's1.jpg', 'u1.jpg', 'v1.jpg', 'w1.jpg', 'w2.jpg']
+for file_name in file_names:
+    image_color = load_image(folder_path + '/' + file_name)
+    img = image_bin(image_gray(image_color))
+    selected_regions, letters, distances = select_roi_with_distances(image_color.copy(), img)
+    display_image(selected_regions, True)
+
+    if file_name == 'e1.jpg':
+        distances = np.array(distances).reshape(len(distances), 1)
+        k_means = KMeans(n_clusters=2)
+        k_means.fit(distances)
+        inputs = prepare_for_ann(letters)
+        result = ann.predict(np.array(inputs, np.float32))
+        print(display_result_with_spaces(result, alphabet, k_means))
+    else:
+        inputs = prepare_for_ann(letters)
+        result = ann.predict(np.array(inputs, np.float32))
+        print(display_result(result, alphabet))
+
